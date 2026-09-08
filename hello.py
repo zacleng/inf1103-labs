7de65a9d10a02455919a43191581b6627e3d512d
zacleng <zacleng22@gmail.com>
2026-09-09 00:56:59 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -26,6 +26,10 @@ age = input("Enter Age: ")
 category = input("Enter Content Category: ")
 
 print("\nInstagram Profile")
+print("===================")
 print("Username: ", username)
 print("Age: ", age)
 print("Content Category: ", category)
+
+if int(age) > 40 and category == "fun":
+    print("You are too old what is fun for you??")
