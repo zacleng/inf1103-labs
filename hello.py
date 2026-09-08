e58a3bdf53396e6e41af4e5e09c79d1272a31f59
zacleng <zacleng22@gmail.com>
2026-09-09 00:46:35 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,3 +2,20 @@ print ("==================================")
 print ("Welcome here")
 print ("my first post!")
 print ("==================================")
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100     
+
+print ("Username: ", username)
+print ("Bio: ", bio)
+print ("Followers: ", followers)
+
+followers += 50
+print ("Day 1: ", followers)
+
+followers += 20
+print ("Day 2: ", followers)
+
+followers += 10
+print ("Day 3: ", followers)
